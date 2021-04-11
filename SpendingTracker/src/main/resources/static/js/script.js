ae3abd802b8bb883b4d8e16c6d1304059fd69f8e
Joshua Rose <jlr007@protonmail.com>
2021-04-10 22:46:55 -0400

    Add, Delete, showAll all functioning...next is update functionality

--- SpendingTracker/src/main/resources/static/js/script.js
+++ SpendingTracker/src/main/resources/static/js/script.js
@@ -26,6 +26,24 @@ function getAllPurchases() {
     xhr.send();
 }
 
+function getPurchase(purchaseId) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', `api/purchase/${purchaseId}`);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if(xhr.status===200) {
+                let purchase = JSON.parse(xhr.responseText);
+                displayPurchase(purchase);
+            }
+            else {
+                displayError('Error retrieving purchases: ' + xhr.status);
+            }
+        }
+    }
+
+    xhr.send();
+}
+
 function displayError(msg) {
     var dataDiv = document.getElementById('mainTable');
     dataDiv.textContent = '';
@@ -47,11 +65,53 @@ function displayPurchases(purchases) {
         category.textContent=purchase.category.name;
         tr.appendChild(category);
         let notes = document.createElement('td');
+        notes.colSpan=2;
         notes.textContent=purchase.notes;
         tr.appendChild(notes);
         div.appendChild(tr);
+        tr.id=purchase.id;
+        tr.addEventListener('click', function(event) {
+            event.preventDefault();
+            var purchaseId = tr.id;
+            if (!isNaN(purchaseId) && purchaseId > 0) {
+              getPurchase(purchaseId);
+            }
+          });
     }
     
+}
+
+function displayPurchase(purchase) {
+    let div = document.getElementById('mainTable');
+
+    for(var x = 2; x < div.rows.length;) {
+        div.deleteRow(x);
+    }
+    
+    let deleteButton = document.createElement('input');
+    deleteButton.type='button';
+    deleteButton.name='delete';
+    deleteButton.value='Delete';
+    deleteButton.id='delete';
+
+    let deletetd = document.createElement('td');
+    deletetd.appendChild(deleteButton);
+    
+    let formRow = document.getElementById('formRow');
+    formRow.appendChild(deletetd);
+
+    deleteButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        var purchaseIdToDelete = purchase.id;
+        deletePurchase(purchaseIdToDelete);
+    });
+
+    let formElement = document.getElementById('purchaseForm');
+
+    formElement.cost.value=purchase.amount;
+    formElement.category.value=purchase.category.id;
+    formElement.notes.value=purchase.notes;
+    formElement.submit.value='Update';
 }
 
 function createPurchase(evt) {
@@ -66,6 +126,27 @@ function createPurchase(evt) {
         }
     };
     postPurchase(purchase);
+}
+
+function deletePurchase(id) {
+    console.log('delete pressed');
+    console.log(id);
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('DELETE', `api/purchase/${id}`);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            console.log(xhr.status);
+          if (xhr.status === 204 || xhr.status === 202) {
+            console.log('success deleting purchase');
+            window.location = "index.html";
+          } else {
+            displayError('Error deleting purchase: ' + xhr.status);
+          }
+        }
+      };
+      xhr.setRequestHeader("Content-type", "application/json");
+      xhr.send();
 }
 
 function postPurchase(purchase) {
